2cfb80284ce97c9a7b57cf089057f9a4c48e47e5
Ruixue Li <lrxpnl@gmail.com>
2014-03-28 09:58:47 -0400

    Prepare for HW7 part B: 1. Change entryPoint back to normal 2. Change version to 7 (change eclipse run configration-program arguments, for moving gwt.xml)

--- src/org/luzhanqi/graphics/LuzhanqiEntryPoint.java
+++ src/org/luzhanqi/graphics/LuzhanqiEntryPoint.java
@@ -3,19 +3,23 @@ package org.luzhanqi.graphics;
 import org.luzhanqi.client.LuzhanqiLogic;
 import org.luzhanqi.client.LuzhanqiPresenter;
 import org.game_api.GameApi;
-import org.game_api.GameApi.ContainerConnector;
 import org.game_api.GameApi.Game;
+import org.game_api.GameApi.IteratingPlayerContainer;
 import org.game_api.GameApi.UpdateUI;
 import org.game_api.GameApi.VerifyMove;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class LuzhanqiEntryPoint implements EntryPoint {
-  ContainerConnector container;
+  IteratingPlayerContainer container;
   LuzhanqiPresenter luzhanqiPresenter;
 
   @Override
@@ -31,13 +35,32 @@ public class LuzhanqiEntryPoint implements EntryPoint {
         luzhanqiPresenter.updateUI(updateUI);
       }
     };
-    container = new GameApi.ContainerConnector(game);
+    container = new IteratingPlayerContainer(game, 2);
     LuzhanqiGraphics luzhanqiGraphics = new LuzhanqiGraphics();
     luzhanqiPresenter =
         new LuzhanqiPresenter(luzhanqiGraphics, container);
-    
-    RootPanel.get("mainDiv").add(luzhanqiGraphics);
+    final ListBox playerSelect = new ListBox();
+    playerSelect.addItem("WhitePlayer");
+    playerSelect.addItem("BlackPlayer");   
+    playerSelect.addItem("Viewer");
+    playerSelect.setSelectedIndex(1);
+   // playerSelect.addItem("WhitePlayerDeploy");
+   // playerSelect.addItem("BlackPlayerDeploy");
+    playerSelect.addChangeHandler(new ChangeHandler() {
+      @Override
+      public void onChange(ChangeEvent event) {
+        int selectedIndex = playerSelect.getSelectedIndex();
+        String playerId = selectedIndex == 2 ? GameApi.VIEWER_ID
+            : container.getPlayerIds().get(selectedIndex);
+        container.updateUi(playerId);
+      }
+    });
+    FlowPanel flowPanel = new FlowPanel();
+    flowPanel.add(playerSelect);
+    flowPanel.add(luzhanqiGraphics);    
+    RootPanel.get("mainDiv").add(flowPanel);
     container.sendGameReady();
-
+    //black start
+    container.updateUi(container.getPlayerIds().get(1));
   }
 }
